18fe3bff5900a2415d0c37b0193171b5fb95e254
Chris Regan <rega0051@umn.edu>
2019-07-17 15:19:48 -0500

    Fixed inclinometer return type to avoid compiler warnings.

--- software/src/soc/cal/inclinometer.h
+++ software/src/soc/cal/inclinometer.h
@@ -22,8 +22,8 @@ struct InclineData {
 class Incline {
   public:
     Incline();
-    bool GetAngle(InclineData *InclineDataPtr);
-    bool SetDamping();
+    void GetAngle(InclineData *InclineDataPtr);
+    void SetDamping();
   private:
     int InclineFileDesc_;
     void OpenPort();
