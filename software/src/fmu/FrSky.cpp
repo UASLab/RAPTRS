622f3facfedfaafdb647cfa6f85761d79bf35cd6
Chris Regan <rega0051@umn.edu>
2019-11-19 15:13:55 -0600

    Commenting FrSky functions.

--- software/src/fmu/FrSky.cpp
+++ software/src/fmu/FrSky.cpp
@@ -7,7 +7,7 @@
 #include "SoftwareSerial.h"
 #include "FrSkySportSensorRpm.h"
 
-
+// Initialize sensor types
 FrSkySportSensorAss ass;
 FrSkySportSensorFcs fcs;
 FrSkySportSensorRpm rpm;
@@ -20,10 +20,12 @@ void BifrostSetup() {
 }
 
 void BifrostSend(float as, uint8_t id, float v, bool soc, uint8_t sel, float ext) {
+   // Set data to fields: FrSky field (Custom field)
+   // Change sensor names on transmitter to match custom field
    ass.setData(as); // Airspeed (Speed)
    fcs.setData(id, v); // Current (TestID), Volage (Vbatt)
-   rpm.setData(0, sel, soc); //----, Temp 1 (Ctrl Sel), Temp 2 (Soc Eng)
-   vario.setData(0, ext); //----, Vertical Speed (Excite Eng)
+   rpm.setData(0, sel, soc); // RPM (), Temp 1 (Ctrl Sel), Temp 2 (Soc Eng)
+   vario.setData(0, ext); // Altitude (), Vertical Speed (Excite Eng)
 
   // Send the telemetry data, note that the data will only be sent for sensors
   // that are being polled at given moment
