501400687ec8dc841147c6b350b873daf14c0cd7
Chris Regan <rega0051@umn.edu>
2020-05-05 15:26:23 -0500

    SerialLink.py broken out as a stand-alone file.

--- software/src/messages/pydemo.py
+++ software/src/messages/pydemo.py
@@ -1,200 +1,41 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Copyright (c) 2016 - 2020 Regents of the University of Minnesota.
+MIT License; See LICENSE.md for complete details
+Author: Chris Regan
+'''
 
 import time
 import os
 
 import fmu_messages
+from SerialLink import SerialLink
+
+# Act as the FMU side of the FMU-SOC comms.
+# SOC will connect to ptySimSoc : 
+
+# Start a Virtual link for a psuedo-tty, SIL
+# socat -vx -d -d PTY,link=ptySimSoc,rawer PTY,link=ptySimFmu,rawer; stty sane;
+
+# Start a Virtual link for a psuedo-tty through BBB USB, HIL (FIXIT - ??)
+#socat -vx -d udp4-datagram:127.0.0.1:6223 PTY,link=ptySimSoc,rawer,nonblock,crnl | 
+#socat -vx -d PTY,link=ptySimSoc,rawer,nonblock,crnl udp4-datagram:127.0.0.1:6222
 
 
-#%% Message Framing
-# Every message should be framed (start and end) with 0x7E (b'~')
-# The escape byte is 0x7D
-# The invert byte for escape is 0x20
-frameEdge = 0x7E; frameEdgeByte = bytes.fromhex('7E');
-frameEscape = 0x7D; frameEscapeByte = bytes.fromhex('7D');
-frameInvert = 0x20; frameInvertByte = bytes.fromhex('20');
+# SOC will request the FMU to Config Mode, then Run Mode
 
-def UnescapeMessage(msg):    
-    msgUnEsc = b''
-    escPrevFlag = False
-    for char in msg:
-        if not escPrevFlag:
-            if char == frameEscape:
-                escPrevFlag = True
-            else:
-                msgUnEsc += char.to_bytes(1, 'little')
-        else:
-            msgUnEsc += (char ^ frameInvert).to_bytes(1, 'little')
-            escPrevFlag = False
-
-    if escPrevFlag:
-        raise ValueError('Invalid message, ends in escape byte')
-
-    return msgUnEsc
-
-
-def EscapeMessage(msg):    
-    msgEsc = b''
-    for char in msg:
-        if char in (frameEdgeByte, frameEscapeByte):
-            msgEsc += frameEscapeByte + (char ^ frameInvert).to_bytes(1, 'little')
-        else:
-            msgEsc += char.to_bytes(1, 'little')
-            
-    return msgEsc
-
-
-#%% CRC
-def crc16(data, crc=0):
-    
-    table = [
-        0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50a5, 0x60c6, 0x70e7,
-        0x8108, 0x9129, 0xa14a, 0xb16b, 0xc18c, 0xd1ad, 0xe1ce, 0xf1ef,
-        0x1231, 0x0210, 0x3273, 0x2252, 0x52b5, 0x4294, 0x72f7, 0x62d6,
-        0x9339, 0x8318, 0xb37b, 0xa35a, 0xd3bd, 0xc39c, 0xf3ff, 0xe3de,
-        0x2462, 0x3443, 0x0420, 0x1401, 0x64e6, 0x74c7, 0x44a4, 0x5485,
-        0xa56a, 0xb54b, 0x8528, 0x9509, 0xe5ee, 0xf5cf, 0xc5ac, 0xd58d,
-        0x3653, 0x2672, 0x1611, 0x0630, 0x76d7, 0x66f6, 0x5695, 0x46b4,
-        0xb75b, 0xa77a, 0x9719, 0x8738, 0xf7df, 0xe7fe, 0xd79d, 0xc7bc,
-        0x48c4, 0x58e5, 0x6886, 0x78a7, 0x0840, 0x1861, 0x2802, 0x3823,
-        0xc9cc, 0xd9ed, 0xe98e, 0xf9af, 0x8948, 0x9969, 0xa90a, 0xb92b,
-        0x5af5, 0x4ad4, 0x7ab7, 0x6a96, 0x1a71, 0x0a50, 0x3a33, 0x2a12,
-        0xdbfd, 0xcbdc, 0xfbbf, 0xeb9e, 0x9b79, 0x8b58, 0xbb3b, 0xab1a,
-        0x6ca6, 0x7c87, 0x4ce4, 0x5cc5, 0x2c22, 0x3c03, 0x0c60, 0x1c41,
-        0xedae, 0xfd8f, 0xcdec, 0xddcd, 0xad2a, 0xbd0b, 0x8d68, 0x9d49,
-        0x7e97, 0x6eb6, 0x5ed5, 0x4ef4, 0x3e13, 0x2e32, 0x1e51, 0x0e70,
-        0xff9f, 0xefbe, 0xdfdd, 0xcffc, 0xbf1b, 0xaf3a, 0x9f59, 0x8f78,
-        0x9188, 0x81a9, 0xb1ca, 0xa1eb, 0xd10c, 0xc12d, 0xf14e, 0xe16f,
-        0x1080, 0x00a1, 0x30c2, 0x20e3, 0x5004, 0x4025, 0x7046, 0x6067,
-        0x83b9, 0x9398, 0xa3fb, 0xb3da, 0xc33d, 0xd31c, 0xe37f, 0xf35e,
-        0x02b1, 0x1290, 0x22f3, 0x32d2, 0x4235, 0x5214, 0x6277, 0x7256,
-        0xb5ea, 0xa5cb, 0x95a8, 0x8589, 0xf56e, 0xe54f, 0xd52c, 0xc50d,
-        0x34e2, 0x24c3, 0x14a0, 0x0481, 0x7466, 0x6447, 0x5424, 0x4405,
-        0xa7db, 0xb7fa, 0x8799, 0x97b8, 0xe75f, 0xf77e, 0xc71d, 0xd73c,
-        0x26d3, 0x36f2, 0x0691, 0x16b0, 0x6657, 0x7676, 0x4615, 0x5634,
-        0xd94c, 0xc96d, 0xf90e, 0xe92f, 0x99c8, 0x89e9, 0xb98a, 0xa9ab,
-        0x5844, 0x4865, 0x7806, 0x6827, 0x18c0, 0x08e1, 0x3882, 0x28a3,
-        0xcb7d, 0xdb5c, 0xeb3f, 0xfb1e, 0x8bf9, 0x9bd8, 0xabbb, 0xbb9a,
-        0x4a75, 0x5a54, 0x6a37, 0x7a16, 0x0af1, 0x1ad0, 0x2ab3, 0x3a92,
-        0xfd2e, 0xed0f, 0xdd6c, 0xcd4d, 0xbdaa, 0xad8b, 0x9de8, 0x8dc9,
-        0x7c26, 0x6c07, 0x5c64, 0x4c45, 0x3ca2, 0x2c83, 0x1ce0, 0x0cc1,
-        0xef1f, 0xff3e, 0xcf5d, 0xdf7c, 0xaf9b, 0xbfba, 0x8fd9, 0x9ff8,
-        0x6e17, 0x7e36, 0x4e55, 0x5e74, 0x2e93, 0x3eb2, 0x0ed1, 0x1ef0,
-        ]
-    
-    for byte in data:
-        crc = ((crc << 8) & 0xff00) ^ table[((crc >> 8) & 0xff) ^ byte]
-    
-    return crc & 0xffff
-
-
-#%% SerialLink
-import serial
-    
-## Serial Framing:
-# FrameEdge(1) + MsgType(1) + ID(1) + Address(1) + Payload + CRC(2) + FrameEdge(1)
-
-class SerialLink():
-    
-    ser = serial.Serial()
-    ser.timeout = 0
-    readBufList = []
-    
-    def __init__ (self, port):
-        self.ser.port = port
-    
-    def begin(self):
-        # Open flush, probably overboard.
-        if self.ser.isOpen():
-            self.ser.flush()
-            self.ser.close()
-        self.ser.open()
-        self.ser.flush()
-
-    def write(self, msgType, msgID, msgAddress, msgPayload):
-        
-        # print('Message Send: ' + '\tType: ' + str(msgType) + '\tID: ' + str(msgID) + '\tAddress: ' + str(msgAddress) + '\tPayload: ' + str(msgPayload))
-        
-        # Join msgData as: Type + ID + Address + Payload
-        msgData = b''
-        msgData += msgType.to_bytes(1, byteorder = 'little')
-        msgData += msgID.to_bytes(1, byteorder = 'little')
-        msgData += msgAddress.to_bytes(1, byteorder = 'little')
-        msgData += msgPayload
-        
-        # Compute CRC
-        crcBytes = crc16(msgData).to_bytes(2, byteorder = 'little')
-        
-        # Message: Framed, Escaped, with CRC
-        msgBytes = frameEdgeByte + EscapeMessage(msgData + crcBytes) + frameEdgeByte
-        
-        # Write to serial
-        numBytes = self.ser.write(msgBytes)
-        
-        return numBytes
-        
-    
-    def checkReceived(self):
-        # Read everything out of the serial
-        if (self.ser.in_waiting > 0):
-            msgRecv = self.ser.read(self.ser.in_waiting)
-        
-            # multiple messages could have been read, Split on edge framing
-            msgLines = msgRecv.split(frameEdgeByte)
-            
-             # Remove empty, append to readBufList
-            [self.readBufList.append(msg) for msg in msgLines if b'' is not msg]
-        
-        return len(self.readBufList)
-    
-    
-    def available(self):
-        return len(self.readBufList)
-    
-
-    def read(self):
-        msgType = -1
-        msgID = -1
-        msgAddress = -1
-        msgPayload = b''
-            
-         # Pop the oldest message off front
-        msgBytes = self.readBufList.pop(0)
-        
-        # print('Raw Recv: ' + str(msgBytes))
-        
-        # Un-Escape the Message
-        msgBytes = UnescapeMessage(msgBytes)
-                
-        # Check CRC
-        msgData = msgBytes[:-2]
-        msgCRC = int.from_bytes(msgBytes[-2:], byteorder = 'little') # Last 2 bytes as integer
-    
-        if (crc16(msgData) == msgCRC):
-            # Split msgData into: Type + ID + Address + Payload
-            msgType = int.from_bytes(msgData[0:1], byteorder = 'little') # Message ID
-            if msgType == 0: # Command type
-                msgID = int.from_bytes(msgData[1:2], byteorder = 'little') # Message ID
-                msgAddress = int.from_bytes(msgData[2:3], byteorder = 'little') # Address - FIXIT
-                msgPayload = msgData[3:]
-                
-                # print('Command Recv: ' + '\tID: ' + str(msgID) + '\tAddress: ' + str(msgAddress) + '\tPayload: ' + str(msgPayload))
-                
-            elif msgType == 1: # Ack type
-                # print('Ack Recv')
-                pass
-            elif msgType == 2: # Nack type
-                # print('Nack Recv')
-                pass
-            else:
-                print('Unknown Type Recv: ' + str(msgBytes))
-                    
-        else:
-            print('\tCRC Fail: ' + str(msgBytes))
-
-        
-        return msgType, msgID, msgAddress, msgPayload
-   
+#Config:
+#1) Read config messages
+#
+#Run:
+#1) Read Sensors (JSBSim source, or otherwise faked)
+#2) Send Sensor messages to SOC
+#3) Read effector messages from SOC
+#4) Send effector commands (JSBSim)
+#5) Step Simulation (X times, for 20ms worth)
+#6) wait until 20ms (from Sensor read)
 
 #%%
 class AircraftSocComms():
@@ -209,9 +50,17 @@ class AircraftSocComms():
         
         return True
         
-    def SendMessage(self, msgType, msgID, msgAddress, msgPayload):
-
-        self.serialLink.write(msgType, msgID, msgAddress, msgPayload);
+    def SendMessage(self, msgID, msgAddress, msgPayload):
+        msgType = 0
+        # print('Message Send: ' + '\tID: ' + str(msgID) + '\tAddress: ' + str(msgAddress) + '\tPayload: ' + str(msgPayload))
+        
+        # Join msgData as: ID + Address + Payload
+        msgData = b''
+        msgData += msgID.to_bytes(1, byteorder = 'little')
+        msgData += msgAddress.to_bytes(1, byteorder = 'little')
+        msgData += msgPayload
+        
+        self.serialLink.write(msgType, msgData);
         
         return True
         
@@ -220,9 +69,24 @@ class AircraftSocComms():
         return self.serialLink.available()
         
     def ReceiveMessage(self):
-        msgType, msgID, msgAddress, msgPayload = self.serialLink.read()
+        msgID = None
+        msgAddress = None
+        msgPayload = None
+        
+        msgType, msgData = self.serialLink.read()
+        
+        if msgType == 0: # command type message
+            # Data = ID + Address + Payload
+            msgID = int.from_bytes(msgData[0:1], byteorder = 'little') # Message ID
+            msgAddress = int.from_bytes(msgData[1:2], byteorder = 'little') # Address
+            msgPayload = msgData[2:]
+            
+            # Send an SerialLink Ack - FIXIT - move to SerialLink
+            self.serialLink.sendStatus(True)
 
-        return msgType, msgID, msgAddress, msgPayload
+        # print('Message Recv: ' + '\tID: ' + str(msgID) + '\tAddress: ' + str(msgAddress) + '\tPayload: ' + str(msgPayload))
+        
+        return msgID, msgAddress, msgPayload
         
     def SendAck(self, msgID, msgSubID = 0): # This is a Config Message Ack (not a SerialLink Ack!)
         
@@ -230,19 +94,12 @@ class AircraftSocComms():
         configMsgAck.ack_id = msgID
         configMsgAck.ack_subid = msgSubID
         
-        self.SendMessage(0, configMsgAck.id, 0, configMsgAck.pack())
+        self.SendMessage(configMsgAck.id, 0, configMsgAck.pack())
 
         return True
 
 
 #%%
-# SOC connects to ptySimSoc : 
-
-#socat -vx -d udp4-datagram:127.0.0.1:6223 PTY,link=ptySimSoc,raw,nonblock,echo=0,b1500000,crnl | 
-#socat -vx -d PTY,link=ptySimSoc,raw,nonblock,echo=0,b1500000,crnl udp4-datagram:127.0.0.1:6222
-
-# Start a Virtual link for a psuedo-tty, SIL
-# socat -vx -d -d PTY,link=ptySimFmu,rawer PTY,link=ptySimSoc,rawer; stty sane;
 
 SocComms = AircraftSocComms(port = 'ptySimFmu')
 fmuMode = 'Config'
@@ -271,9 +128,11 @@ dataMsgSwift = fmu_messages.data_swift()
 dataMsgSbus = fmu_messages.data_sbus()
 
 
+tRate_s = 1/50 # Desired Run rate
 SocComms.Begin()
-
 while (True):
+    tStart_s = time.time()
+        
     # Run Stuff
     # Send Bifrost Data...
     # FIXIT
@@ -299,7 +158,11 @@ while (True):
     
     # Check and Recieve Messages
     while(SocComms.CheckMessage()):
-        msgType, msgID, msgAddress, msgPayload = SocComms.ReceiveMessage()
+        msgID, msgAddress, msgPayload = SocComms.ReceiveMessage()
+        
+        # Message did not have an ID, likely a SerialLink Ack/Nack message
+        if msgID is None:
+            continue
         
         if msgID == fmu_messages.command_mode_id: # request mode
                     
@@ -330,18 +193,28 @@ while (True):
                          cfgMsgAms5915.id, cfgMsgSwift.id, cfgMsgAnalog.id, cfgMsgEffector.id, 
                          cfgMsgMission.id, cfgMsgControlGain.id]
             
-            if msgType == 0:
-                if msgID in cfgIdList: 
+            if msgID in cfgIdList: 
+            
+                # FIXIT - do something with the config messages!!
                 
-                    # FIXIT - do something with the config messages!!
-                    
-                    print("Config Set: " + str(msgPayload))
-                    
-                    SocComms.SendAck(msgID);
-                else:
-                    print("Unhandled message while in Configuration mode, id: " + str(msgID))
-            # if msgType
+                print("Config Set: " + str(msgPayload))
+                
+                SocComms.SendAck(msgID);
+            else:
+                print("Unhandled message while in Configuration mode, id: " + str(msgID))
+
         # if 
+    #
+    
+    # Timer - attempt...
+    tHold_s = tRate_s - (time.time() - tStart_s)
+    if tHold_s > 0:
+        time.sleep(tHold_s)
+        
+#    print('Time (ms): ' + str((time.time() - tStart_s) * 1000))
+        
+# end while(True)
         
 #SocComms.Close()
+
 
